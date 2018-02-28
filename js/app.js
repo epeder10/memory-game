ea61467ee6f8f7cfb147f81dbc1307a29e015b8a
epeder10 <epeder10@gmail.com>
2018-02-28 13:47:06 -0500

    REFACTOR pretify

--- js/app.js
+++ js/app.js
@@ -1,27 +1,51 @@
 /*
  * Global variables
  */
-var cardNames = ['fa-diamond', 'fa-paper-plane-o', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-anchor', 'fa-leaf', 'fa-bicycle', 'fa-diamond', 'fa-bomb', 'fa-leaf', 'fa-bomb', 'fa-bolt', 'fa-bicycle', 'fa-paper-plane-o', 'fa-cube'];
+var cardNames = [
+  "fa-diamond",
+  "fa-paper-plane-o",
+  "fa-anchor",
+  "fa-bolt",
+  "fa-cube",
+  "fa-anchor",
+  "fa-leaf",
+  "fa-bicycle",
+  "fa-diamond",
+  "fa-bomb",
+  "fa-leaf",
+  "fa-bomb",
+  "fa-bolt",
+  "fa-bicycle",
+  "fa-paper-plane-o",
+  "fa-cube"
+];
 var matchedBoxes = 0;
 var firstSelection = null;
 var secondSelection = null;
 var time = null;
 var timer = 0;
 
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("the DOM is ready to be interacted with!");
+});
+
 //Main game loop
 timer = setInterval(updateTime, 500);
-document.querySelector('.deck').addEventListener('click', onClick);
+document.querySelector(".deck").addEventListener("click", onClick);
 
 // Main card click function for each card.
 function onClick(event) {
-  if (event.target.nodeName === 'LI') {
+  if (event.target.nodeName === "LI") {
     // display the card's symbol
-    if (event.target.className !== 'card open show') {
-      event.target.className += ' open show';
+    if (event.target.className !== "card open show") {
+      event.target.className += " open show";
       // if this is the first card selection
       if (firstSelection === null) {
         firstSelection = event.target;
-      } else if (firstSelection.firstElementChild.className != event.target.firstElementChild.className) {
+      } else if (
+        firstSelection.firstElementChild.className !=
+        event.target.firstElementChild.className
+      ) {
         // this is the second card selection and the two selections are not equal
         secondSelection = event.target;
         // Wait one second so the user can see the wrong match.  Then flip the cards back.
@@ -40,19 +64,19 @@ function onClick(event) {
  * the number of moves
  */
 function clearCards() {
-  firstSelection.className = 'card';
-  secondSelection.className = 'card';
+  firstSelection.className = "card";
+  secondSelection.className = "card";
   firstSelection = secondSelection = null;
   addMove();
 }
 
 /*
- * We have a match.  Mark them as perm matches and increment the number of matched matchedBoxes
+ * We have a match.  Mark them as perm matches and increment the number of matched boxes
  * Check if all boxes have been matched.  If so. End game.
  */
 function foundMatch() {
-  firstSelection.className = 'card match';
-  secondSelection.className = 'card match';
+  firstSelection.className = "card match";
+  secondSelection.className = "card match";
   firstSelection = secondSelection = null;
   addMove();
   updateNumberOfMatches();
@@ -66,32 +90,38 @@ function foundMatch() {
  */
 function endGame() {
   clearInterval(timer);
-  var moves = document.querySelector('.moves').textContent;
-  alert("You win! You beat the puzzle in " + getTime() + " It took you " + moves + " turns to beat this puzzle. Try to beat your score!");
+  var moves = document.querySelector(".moves").textContent;
+  alert(
+    "You win! You beat the puzzle in " +
+      getTime() +
+      " It took you " +
+      moves +
+      " turns to beat this puzzle. Try to beat your score!"
+  );
 }
 
 /*
  * Maintain a number of boxes that have been matched
  */
 function updateNumberOfMatches() {
-  matchedBoxes = document.querySelectorAll('.match').length;
+  matchedBoxes = document.querySelectorAll(".match").length;
 }
 
 /*
  * Reset the game
  */
-document.querySelector('.restart').addEventListener('click', function(event) {
+function reset() {
   //get all cards from the DOM
-  var cards = document.querySelectorAll('.card');
+  var cards = document.querySelectorAll(".card");
   //shuffle the cards
   cardNames = shuffle(cardNames);
 
   for (var i = 0; i < cards.length; i++) {
-    cards[i].className = 'card';
-    cards[i].firstElementChild.className = 'fa ' + cardNames[i];
+    cards[i].className = "card";
+    cards[i].firstElementChild.className = "fa " + cardNames[i];
   }
 
-  document.querySelector('.moves').textContent = 0;
+  document.querySelector(".moves").textContent = 0;
   matchedBoxes = 0;
   firstSelection = secondSelection = null;
 
@@ -102,16 +132,21 @@ document.querySelector('.restart').addEventListener('click', function(event) {
 
   //reset stars
   resetStars();
+}
 
-});
+document.querySelector(".restart").addEventListener("click", reset);
 
 /*
  * Increment the number of moves variable
  */
 function addMove() {
-  var moves = document.querySelector('.moves');
+  var moves = document.querySelector(".moves");
   moves.textContent = parseInt(moves.textContent) + 1;
-  if (moves.textContent === '1' || moves.textContent === '24' || moves.textContent === '36'){
+  if (
+    moves.textContent === "1" ||
+    moves.textContent === "24" ||
+    moves.textContent === "36"
+  ) {
     removeStar();
   }
 }
@@ -121,46 +156,52 @@ function addMove() {
  * Convert the time to a standard display timer
  * Hour:Minute:second
  */
-function getTime () {
+function getTime() {
   var tmp = time / 2;
   var h = Math.trunc(tmp / 3600);
-  var m = Math.trunc((tmp - (h * 3600)) / 60);
-  var s = Math.trunc((tmp - (m * 60)) - (h * 3600));
-  return h.toString().padStart(2, '0') + ':' + m.toString().padStart(2, '0') + ':' + s.toString().padStart(2, '0');
+  var m = Math.trunc((tmp - h * 3600) / 60);
+  var s = Math.trunc(tmp - m * 60 - h * 3600);
+  return (
+    h.toString().padStart(2, "0") +
+    ":" +
+    m.toString().padStart(2, "0") +
+    ":" +
+    s.toString().padStart(2, "0")
+  );
 }
 
-function updateTime () {
+function updateTime() {
   time += 1;
-  document.querySelector('.timer').textContent = 'Timer: ' + getTime();
+  document.querySelector(".timer").textContent = "Timer: " + getTime();
 }
 
 /*
  * Add stars if less than 3
  */
-function resetStars(){
-  var stars = document.querySelector('.stars');
-  while (stars.children.length < 3){
-    const li = document.createElement('li');
+function resetStars() {
+  var stars = document.querySelector(".stars");
+  while (stars.children.length < 3) {
+    const li = document.createElement("li");
     const i = '<i class="fa fa-star"></i>';
-    li.insertAdjacentHTML('afterbegin', i);
+    li.insertAdjacentHTML("afterbegin", i);
 
     stars.appendChild(li);
   }
-
 }
 
 /*
  * Every 12 moves remove a star
  */
-function removeStar(){
-  var stars = document.querySelector('.stars');
+function removeStar() {
+  var stars = document.querySelector(".stars");
   stars.removeChild(stars.children[0]);
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
   var currentIndex = array.length,
-    temporaryValue, randomIndex;
+    temporaryValue,
+    randomIndex;
 
   while (currentIndex !== 0) {
     randomIndex = Math.floor(Math.random() * currentIndex);
