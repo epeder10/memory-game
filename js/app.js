22535a58cc4d08120ea1b053be371cc0cfd0b80a
Eric Pedersen <epeder10@gmail.com>
2018-02-27 22:54:28 -0500

    Code refactor for readability

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
  * Create a list that holds all of your cards
  */
 var cards = document.querySelectorAll('.card');
-var card_names = ["fa-diamond","fa-paper-plane-o","fa-anchor","fa-bolt","fa-cube","fa-anchor","fa-leaf","fa-bicycle","fa-diamond","fa-bomb","fa-leaf","fa-bomb","fa-bolt","fa-bicycle","fa-paper-plane-o","fa-cube"];
+var card_names = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-anchor", "fa-leaf", "fa-bicycle", "fa-diamond", "fa-bomb", "fa-leaf", "fa-bomb", "fa-bolt", "fa-bicycle", "fa-paper-plane-o", "fa-cube"];
 var matched_boxes = 0;
 var firstSelection = null;
 var secondSelection = null;
@@ -13,13 +13,13 @@ var secondSelection = null;
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
-document.querySelector('.restart').addEventListener('click', function(event){
+document.querySelector('.restart').addEventListener('click', function(event) {
   var deck = document.querySelector('.deck');
 
   card_names = shuffle(card_names);
 
-  for(var i = 0; i < cards.length; i++){
-    cards[i].className='card';
+  for (var i = 0; i < cards.length; i++) {
+    cards[i].className = 'card';
     cards[i].firstElementChild.className = 'fa ' + card_names[i];
 
   }
@@ -27,37 +27,37 @@ document.querySelector('.restart').addEventListener('click', function(event){
   document.querySelector('.moves').textContent = 0;
 
   matched_boxes = 0;
-  firstSelection = null;
-  secondSelection = null;
+  firstSelection = secondSelection = null;
 
 });
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 /*
  * Maintain a number of boxes that have been matched
  */
-function updateNumberOfMatches(){
+function updateNumberOfMatches() {
   matched_boxes = document.querySelectorAll('.match').length;
 }
 
 /*
  * Increment the number of moves variable
  */
-function addMove(){
+function addMove() {
   var moves = document.querySelector('.moves');
   moves.textContent = parseInt(moves.textContent) + 1;
 }
@@ -66,11 +66,10 @@ function addMove(){
  * Flip the cards back over and reset the selections.  Increment
  * the number of moves
  */
-function clearCards(){
+function clearCards() {
   firstSelection.className = 'card';
   secondSelection.className = 'card';
-  firstSelection = null;
-  secondSelection = null;
+  firstSelection = secondSelection = null;
   addMove();
 }
 
@@ -78,33 +77,40 @@ function clearCards(){
  * We have a match.  Mark them as perm matches and increment the number of matched matched_boxes
  * Check if all boxes have been matched.  If so. End game.
  */
-function foundMatch(){
+function foundMatch() {
   firstSelection.className = 'card match';
   secondSelection.className = 'card match';
-  firstSelection = null;
-  secondSelection
+  firstSelection = secondSelection = null;
   addMove();
   updateNumberOfMatches();
-  if(matched_boxes === 16){
+  if (matched_boxes === 16) {
     endGame();
   }
 }
 
-function endGame(){
+/*
+ * The game has been won.  Now end it.
+ */
+function endGame() {
   var moves = document.querySelector('.moves').textContent;
   alert("You win! It took you " + moves + " turns to beat this puzzle. Now try to beat it!");
 }
+
 // Click function for each card.
-function onClick(event){
-  if(event.target.nodeName === 'LI'){
+function onClick(event) {
+  if (event.target.nodeName === 'LI') {
+    //- display the card's symbol
     event.target.className += ' open show';
-    if(firstSelection === null){
+    //if this is the first card selection
+    if (firstSelection === null) {
       firstSelection = event.target;
-    }else if(firstSelection.firstElementChild.className != event.target.firstElementChild.className){
+    } else if (firstSelection.firstElementChild.className != event.target.firstElementChild.className) {
+      //this is the second card selection and the two selections are not equal
       secondSelection = event.target;
       //Wait one second so the user can see the wrong match.  Then flip the cards back.
       tmout = setTimeout(clearCards, 300);
-    }else{
+    } else {
+      //correct selection 
       secondSelection = event.target;
       foundMatch();
     }
